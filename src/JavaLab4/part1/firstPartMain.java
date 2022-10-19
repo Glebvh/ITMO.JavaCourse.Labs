75d1ed94b24d4105fed87b6c45747b6066969d00
glebv <glebv@inbox.ru>
2022-10-19 21:48:46 +0300

    + fixes

--- src/JavaLab4/part1/firstPartMain.java
+++ src/JavaLab4/part1/firstPartMain.java
@@ -18,12 +18,18 @@ public class firstPartMain {
         //1.6
         MethodSix();
     }
+
+    //	Напишите программу, которая выводит на консоль нечетные числа от 1 до 99.
+    //	Каждое число печатается с новой строки
     public static void MethodOne(){
         for (int i = 1;i <= 100; i++){
             System.out.println(i);
         }
     }
-
+    // Напишите программу, которая выводит числа от 1 до 100, которые делятся на 3, 5 и на то и другое (то есть и на 3 и на 5). Пример вывода:
+    //	Делится на 3: ….
+    //	Делится на 5: ….
+    //	Делится на 3 и на 5: ….
     public static void MethodTwo(){
         for (int j = 1;j <= 100; j++) {
             if (j % 3 == 0)
@@ -38,7 +44,12 @@ public class firstPartMain {
             }
         }
     }
-
+    //Напишите программу, чтобы вычислить сумму двух целых чисел и вернуть true,
+    // если сумма равна третьему целому числу. Пример вывода:
+    //Введите первое число: 5
+    //Введите второе число: 10
+    //Введите третье число: 15
+    //Результат: true
     public static void MethodThree(){
         Scanner in = new Scanner(System.in);
         System.out.println("Insert first number");
@@ -51,7 +62,12 @@ public class firstPartMain {
             System.out.println("Result is true");
         } else System.out.println("Result is false");
     }
-
+    //Напишите программу, которая принимает от пользователя три целых числа и возвращает true,
+    // если второе число больше первого числа, а третье число больше второго числа. Пример вывода:
+    //Введите первое число: 5
+    //Введите второе число: 10
+    //Введите третье число: 15
+    //Результат: true
     public static void MethodFour(){
         Scanner in = new Scanner(System.in);
         System.out.println("Insert first number");
@@ -65,6 +81,11 @@ public class firstPartMain {
             } else
                 System.out.println("Result is false");
     }
+
+    //Напишите программу, чтобы проверить, появляется ли число 3 как первый или последний элемент массива целых чисел.
+    // Длина массива должна быть больше или равна двум. Пример вывода:
+    //array = 3, -3, 7, 4, 5, 4, 3
+    //true
     public static void MethodFive(){
         int[] array = new int[5];
         array[0] = 3;
@@ -79,7 +100,7 @@ public class firstPartMain {
             System.out.println("False");
         }
     }
-
+    //Напишите программу, чтобы проверить, что массив содержит число 1 или 3.
     public static void MethodSix() {
         int[] array = new int[] {66,88,88,5,88,99,88,1};
         Boolean result = false;
