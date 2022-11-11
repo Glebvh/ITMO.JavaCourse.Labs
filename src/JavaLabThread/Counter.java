54ac92d5cca5eca5439229619769860838c84298
glebv <glebv@inbox.ru>
2022-11-11 22:16:37 +0300

    +Threads fixes with CountDownLatch

--- src/JavaLabThread/Counter.java
+++ src/JavaLabThread/Counter.java
@@ -1,14 +1,15 @@
 package JavaLabThread;
 
+import java.util.concurrent.CountDownLatch;
+
 class Counter extends Thread {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
         startHundredTreads();
-
         System.out.println("result: " + getCount());
-
-
-
     }
+
+    private static final CountDownLatch cdl = new CountDownLatch(100);
     static int count = 0;
 
     public static void increment() {
@@ -19,18 +20,20 @@ class Counter extends Thread {
         return count;
     }
 
-    public static void startHundredTreads() {
+    public static void startHundredTreads() throws InterruptedException {
         for (int i = 0; i < 100; i++) {
             new Counter().start();
         }
+        cdl.await();
     }
 
     @Override
     public void run() {
         synchronized (Counter.class) {
             for (int i = 1; i <= 1000; i++) {
-                Counter.increment();
+                increment();
             }
+            cdl.countDown();
         }
     }
 }
